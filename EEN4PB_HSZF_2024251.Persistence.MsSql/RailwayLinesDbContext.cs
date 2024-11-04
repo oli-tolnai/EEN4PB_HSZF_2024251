cc61eec3dd392100e5f0ed3fe1a4eddcfeace346
oli-tolnai <oli.tolnai3@gmail.com>
2024-11-04 21:35:10 +0100

    RailwayLinesDbContext OnConfiguring done

--- EEN4PB_HSZF_2024251.Persistence.MsSql/RailwayLinesDbContext.cs
+++ EEN4PB_HSZF_2024251.Persistence.MsSql/RailwayLinesDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EEN4PB_HSZF_2024251.Model;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace EEN4PB_HSZF_2024251.Persistence.MsSql
@@ -10,5 +11,28 @@ namespace EEN4PB_HSZF_2024251.Persistence.MsSql
         {
             builder.AddConsole();
         });
+
+
+        public DbSet<RailwayLine> RailwayLines { get; set; }
+
+        public DbSet<Service> Services { get; set; }
+
+        //Constructor, which is used to delete and create the database
+        public RailwayLinesDbContext()
+        {
+            this.Database.EnsureDeleted();
+            this.Database.EnsureCreated();
+        }
+
+        //OnConfiguring method is used to configure the database connection
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connStr = @"Server=(localdb)\\MSSQLLocalDB;Database=RailwayLineDb;Trusted_Connection=True;TrustServerCertificate=True";
+
+            optionsBuilder
+                .UseLazyLoadingProxies()
+                .UseLoggerFactory(MyLoggerFactory)
+                .UseSqlServer(connStr);
+        }
     }
 }
