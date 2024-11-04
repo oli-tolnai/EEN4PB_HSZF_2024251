d2076f33375d240a0c7ee2640df38e4fa166e3b4
oli-tolnai <oli.tolnai3@gmail.com>
2024-11-04 21:20:29 +0100

    ILoggerFactory

--- EEN4PB_HSZF_2024251.Persistence.MsSql/RailwayLinesDbContext.cs
+++ EEN4PB_HSZF_2024251.Persistence.MsSql/RailwayLinesDbContext.cs
@@ -1,9 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace EEN4PB_HSZF_2024251.Persistence.MsSql
 {
     public class RailwayLinesDbContext : DbContext
     {
-
+        //Write things to the console
+        public static readonly ILoggerFactory MyLoggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder.AddConsole();
+        });
     }
 }
