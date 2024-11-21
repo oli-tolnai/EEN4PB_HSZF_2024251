d2f20488a2e67f5d5fa2c7bfc14cf2921bbfe7ef
oli-tolnai <oli.tolnai3@gmail.com>
2024-11-21 21:28:29 +0100

    cleanup, unnecessary commented out codes removed

--- EEN4PB_HSZF_2024251.Console/Program.cs
+++ EEN4PB_HSZF_2024251.Console/Program.cs
@@ -14,27 +14,11 @@ namespace EEN4PB_HSZF_2024251
     {
         static void Main(string[] args)
         {
-            /*
-            var ctx = new RailwayLinesDbContext();
-            var RdataProvider = new RailwayLinesDataProvider(PathInput(), ctx);
-
-            var servicesDataProvider = new ServicesDataProvider(ctx);
-            var q1 = servicesDataProvider.GetServices();
-            var id = q1[1].Id;
-            var q2 = servicesDataProvider.GetServiceById(id);
-            var q3 = servicesDataProvider.GetRailwayLineServices("120A", "BP-Keleti->Szolnoks");
-
-            */
-
-            
-
-
             var host = Host.CreateDefaultBuilder()
                 .ConfigureServices((hostContext, services) =>
                 {
                     services.AddScoped<RailwayLinesDbContext>();
                     services.AddSingleton<IRailwayLinesDataProvider, RailwayLinesDataProvider>();
-                    //services.AddSingleton<IRailwayLinesDataProvider>(provider => new RailwayLinesDataProvider(Menu.FirstMenu(), provider.GetRequiredService<RailwayLinesDbContext>()));
                     services.AddSingleton<IRailwayLinesLogic, RailwayLinesLogic>();
 
                     services.AddSingleton<IServicesDataProvider, ServicesDataProvider>();
@@ -45,14 +29,7 @@ namespace EEN4PB_HSZF_2024251
 
             using IServiceScope serviceScope = host.Services.CreateScope();
 
-            
-            //IRailwayLinesDataProvider railwayProvider = host.Services.GetRequiredService<IRailwayLinesDataProvider>();
-
             IRailwayLinesLogic railwayLogic = host.Services.GetRequiredService<IRailwayLinesLogic>();
-
-            //IServicesDataProvider serviceProvider = host.Services.GetRequiredService<IServicesDataProvider>();
-
-            //List<RailwayLine> q11 = railwayLogic.GetRailwayLines();
 
             railwayLogic.FillDatabaseWithNewData(Menu.FirstMenu());
 
@@ -61,7 +38,7 @@ namespace EEN4PB_HSZF_2024251
 
 
 
-            /*
+            /* TESTS
             var q1 = serviceProvider.GetServices();
             var id = q1[1].Id;
             var q2 = serviceProvider.GetServiceById(id);
--- EEN4PB_HSZF_2024251.Persistence.MsSql/RailwayLinesDataProvider.cs
+++ EEN4PB_HSZF_2024251.Persistence.MsSql/RailwayLinesDataProvider.cs
@@ -36,10 +36,9 @@ namespace EEN4PB_HSZF_2024251.Persistence.MsSql
         public IJsonImportToDb jsonImportToDb = new JsonImportToDb();
         
 
-        public RailwayLinesDataProvider(/*string path,*/ RailwayLinesDbContext ctx)
+        public RailwayLinesDataProvider(RailwayLinesDbContext ctx)
         {
             this.ctx = ctx;
-            //jsonImportToDb!.JsonIntoDb(path, ctx);
         }
 
         public void FillDatabaseFirstTime(string path)
@@ -51,66 +50,5 @@ namespace EEN4PB_HSZF_2024251.Persistence.MsSql
         {
             jsonImportToDb!.NewJsonIntoDb(path, ctx);
         }
-
-
-
-
-
-        /*
-        //SeedDatabase method is used to fill the database with some initial data for testing purposes
-        private void SeedDatabase()
-        {
-            var railwayLines = new List<RailwayLine>
-            {
-                new RailwayLine("BP-Keleti->Szolnok", "120A"),
-                new RailwayLine("BP-Nyugati->Szolnok", "100A")
-            };
-            ctx.RailwayLines.AddRange(railwayLines);
-            ctx.SaveChanges();
-
-            var services = new List<Service>
-            {
-                new Service("Szolnok", "Budapest-Keleti", 3320, 3, "InterCity"),
-                new Service("Budapest-Keleti", "Sülysáp", 3210, 10, "Passenger"),
-                new Service("BP-Nyugati", "Cegléd", 4320, 25, "Passenger")
-            };
-            ctx.Services.AddRange(services);
-            ctx.SaveChanges();
-
-            railwayLines[0].Services.Add(services[0]);
-            railwayLines[0].Services.Add(services[1]);
-            railwayLines[1].Services.Add(services[2]);
-            ctx.SaveChanges();
-        }*/
-
-        /*
-        //JsonDeserialize method is used to deserialize the JSON data
-        private List<RailwayLine> JsonDeserialize(string path)
-        {
-            
-            string jsonString = File.ReadAllText(path);
-            Console.WriteLine(jsonString);
-
-            RailwayData railwayData = JsonConvert.DeserializeObject<RailwayData>(jsonString);
-            
-
-            return railwayData.RailwayLines;
-                
-        }
-
-        //JsonToDb method is used to convert the deserialized JSON data to database entities
-        private void JsonToDb(List<RailwayLine> railwayLines)
-        {            
-            foreach (var railwayLine in railwayLines)
-            {
-                ctx.RailwayLines.Add(railwayLine);
-                foreach (var service in railwayLine.Services)
-                {
-                    ctx.Services.Add(service);
-                }
-            }
-            ctx.SaveChanges();
-            ;
-        }*/
     }
 }
